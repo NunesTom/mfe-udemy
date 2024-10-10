e9d1010b7c56dd5b22c4199ccb2392dc155be804
Ailton Nunes <ailton_nunes_hvar@whirlpool.com>
2024-10-10 19:37:33 -0300

    feat: test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Olá Mundo!!!!</h1>
+      <h1>Olá Mundo, com Bruna e Heitor</h1>
       <hr />
       <MarketingApp />
     </div>
