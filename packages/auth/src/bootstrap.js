6ce17f97a37c29eb1603c82ac0691c48b1d38928
Ailton Nunes <ailton_nunes_hvar@whirlpool.com>
2024-10-14 14:01:28 -0300

    feat: added authPage

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath],
@@ -12,10 +13,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(
-    <App history={history} />,
-    el
-  );
+  ReactDOM.render(<App history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -28,13 +26,16 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   };
 };
 
-
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
